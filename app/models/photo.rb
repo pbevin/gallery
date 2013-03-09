54001e903f04b7c25a12cf89644d6f45b724d08f
Pete Bevin <pete@petebevin.com>
2013-03-09 13:41:40 -0500

    Set default title when uploading

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,11 @@
 class Photo < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+
+  before_create :default_title
+
+  private
+
+  def default_title
+    self.title ||= File.basename(image.filename, ".*").titleize if image
+  end
 end
