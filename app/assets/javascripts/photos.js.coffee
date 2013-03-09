a6989381e0a7301d23c4084d102d32afa8d368df
Pete Bevin <pete@petebevin.com>
2013-03-09 13:26:41 -0500

    Display photos as they are uploaded

--- app/assets/javascripts/photos.js.coffee
+++ app/assets/javascripts/photos.js.coffee
@@ -2,4 +2,5 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 jQuery ->
-  $('#new_photo').fileupload();
+  $('#new_photo').fileupload
+    dataType: "script"
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -24,17 +24,7 @@ class PhotosController < ApplicationController
   # POST /photos
   # POST /photos.json
   def create
-    @photo = Photo.new(photo_params)
-
-    respond_to do |format|
-      if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @photo }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
-      end
-    end
+    @photo = Photo.create(photo_params)
   end
 
   # PATCH/PUT /photos/1
